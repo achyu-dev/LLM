cd9b4888ba8d52115edd66a1ab5d80c7eb657c7f
unknown <93060367+achyu-dev@users.noreply.github.com>
2024-10-26 11:23:41 +0530

    added intial files

---
+++ Class 4 - 30 Aug/lang.py
@@ -0,0 +1,9 @@
+from langchain.prompts import PromptTemplate
+
+prompt_template = PromptTemplate.from_template(
+    "Tell me a {adjective} joke about {content}."
+)
+
+prompt_template.format(adjective="funny", content="bags")
+
+print(prompt_template)
